a8832a92c3b03abce5501a4e5aaa02b972a4eb39
ratiomark <yanagae@gmail.com>
2023-04-04 16:32:52 +0300

    fix after review

--- controllers/users.js
+++ controllers/users.js
@@ -50,7 +50,7 @@ const updateUserProfile = async (req, res) => {
 			{ name, about },
 			{ new: true },
 		);
-		if (!data) throw new DataNotFoundInDb();
+		if (!data) throw new IncorrectData();
 		res.send({ data });
 	} catch (err) {
 		errorsChecker(err, res);
