6a83075d360080fd1b6388d7eab620eefd0d0b26
ratiomark <yanagae@gmail.com>
2023-04-19 13:41:43 +0300

    fix token and auth-middleware

--- validation/validation.js
+++ validation/validation.js
@@ -31,7 +31,8 @@ const patchUserDataValidation = [
 
 const handleValidationErrors = (req, res, next) => {
 	const errors = validationResult(req)
-	if (!errors.isEmpty()) return res.status(400).json(errors.array())
+	if (!errors.isEmpty()) return next(ApiError.BadRequest())
+	// .json(errors.array())
 	next()
 }
 
